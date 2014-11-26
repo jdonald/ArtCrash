9b5dedf5a88bf9d3433cdf3eb57210c58a72a754
James Donald <jdonald@leapmotion.com>
2014-11-26 10:46:12 -0800

    Add example project verified crashing on Nexus 5 + ART

---
+++ Assets/CallDllImport.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class CallDllImport : MonoBehaviour {
+
+  int count = 0;
+	// Use this for initialization
+	void Start () {
+	  Debug.Log("started CallDllImport object");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Leap.Frame f = new Leap.Frame();
+        if (!f.IsValid) {
+          Debug.Log("allocated a Leap.Frame with JNI destructor " + count);
+        }
+    count += 1;
+	}
+}
